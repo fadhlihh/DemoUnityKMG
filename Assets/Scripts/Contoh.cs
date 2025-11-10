3c8a01b5a8be5777fa70effb9cd2b722b64c3e78
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2025-11-10 17:05:48 +0700

    belajar fundamental programming

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+    int nyawa = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log(Tambah(5, 10));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MinumObat()
     {
+        Debug.Log("Pemain meminum obat");
+        Sembuhkan(30);
+    }
 
+    public void Sembuhkan(int tambahanNyawa)
+    {
+        nyawa = nyawa + tambahanNyawa;
+    }
+
+    public int Tambah(int angka1, int angka2)
+    {
+        int result = angka1 + angka2;
+        return result;
     }
 }
